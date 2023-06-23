b569f6e20a3a32ddd739ae76cb3fa696be2650e2
Hardeep <hardeep41016@gmail.com>
2023-06-23 08:23:03 +0530

    adding receipt

--- src/Pages/IncomeTax.jsx
+++ src/Pages/IncomeTax.jsx
@@ -26,7 +26,13 @@ function IncomeTax() {
   const [domCess, setDomCess] = useState("");
   const [forTaxable, setForTaxable] = useState("");
   const [forCess, setForCess] = useState("");
+  const [june, setJune] = useState("");
+  const [sept, setSept] = useState("");
+  const [dec, setDec] = useState("");
+  const [mar, setMar] = useState("");
+  const [mar_2, setMar_2] = useState("");
 
+  const sum = june + june * 2 + june * 2;
   const handlePayer = (e) => {
     setComponent(e.target.value);
   };
@@ -99,10 +105,22 @@ function IncomeTax() {
       setTaxable(Math.round(gross * 0.15));
     } else if (gross > 1200000 && gross < 1500000) {
       setTaxable(Math.round(gross * 0.2));
+    }else{
+      setTaxable(Math.round(gross * 0.3));
     }
-    setCess(Math.round(taxable / 22));
 
-    setResult(cess + taxable);
+    const cs = Math.round(taxable / 22);
+    setCess(cs);
+    const res = cess + taxable;
+
+    setResult(res);
+
+    const j = Math.round(result / 6.66);
+    const s = Math.round(result / 2.22);
+    const d = Math.round(result / 1.33);
+    setJune(j);
+    setSept(s);
+    setDec(d);
   };
 
   const handleHuf = () => {
@@ -117,58 +135,55 @@ function IncomeTax() {
     } else if (salary > 1200000 && salary < 1500000) {
       setTaxable(Math.round(salary * 0.2));
     }
-    setCess(Math.round(taxable / 22))
+    setCess(Math.round(taxable / 22));
 
-    setLiability(taxable + cess)
+    setLiability(taxable + cess);
 
-    setAccessed(liability - relief)
-  }
+    setAccessed(liability - relief);
+  };
 
-  const handleDom= () => {
-    
-    setDomTaxable(+(salary * 0.3))
-    setDomCess(Math.round(domTaxable / 22))
+  const handleDom = () => {
+    setDomTaxable(+(salary * 0.3));
+    setDomCess(Math.round(domTaxable / 22));
 
-    setLiability(domTaxable + domCess)
+    setLiability(domTaxable + domCess);
 
-    setAccessed(liability - relief)
-  }
+    setAccessed(liability - relief);
+  };
 
-  const handleFor= () => {
-    
-    setForTaxable(+(salary * 0.25))
-    setForCess(Math.round(forTaxable / 25))
+  const handleFor = () => {
+    setForTaxable(+(salary * 0.25));
+    setForCess(Math.round(forTaxable / 25));
 
-    setLiability(forTaxable + forCess)
+    setLiability(forTaxable + forCess);
 
-    setAccessed(liability - relief)
-  }
-
+    setAccessed(liability - relief);
+  };
 
   const handleReset = () => {
-    setAccessed('');
-    setAgriculture('');
-    setBac('');
-    setCapital('');
-    setCess('');
-    setComission('');
-    setDeduction('');
-    setDomCess('');
-    setDomTaxable('');
-    setForCess('');
-    setForTaxable('');
-    setHouse('');
-    setInterest('');
-    setLiability('');
-    setLottery('');
-    setOther('');
-    setProfits('');
-    setResult('');
-    setSalary('');
-    setTaxable('');
+    setAccessed("");
+    setAgriculture("");
+    setBac("");
+    setCapital("");
+    setCess("");
+    setComission("");
+    setDeduction("");
+    setDomCess("");
+    setDomTaxable("");
+    setForCess("");
+    setForTaxable("");
+    setHouse("");
+    setInterest("");
+    setLiability("");
+    setLottery("");
+    setOther("");
+    setProfits("");
+    setResult("");
+    setSalary("");
+    setTaxable("");
     setToggleDeduction(false);
     setToggleOtherSource(false);
-  }
+  };
   return (
     <div>
       <div>
@@ -421,12 +436,14 @@ function IncomeTax() {
               <button className="calc_btn" onClick={handleCalulate}>
                 Calculate
               </button>
-              <button className="reset_btn" onClick={handleReset}>Reset</button>
+              <button className="reset_btn" onClick={handleReset}>
+                Reset
+              </button>
             </div>
           </div>
         )}
 
-        {(component === "huf" || component === 'aop') && (
+        {(component === "huf" || component === "aop") && (
           <div>
             <div className="input-div">
               <label htmlFor="">
@@ -445,10 +462,15 @@ function IncomeTax() {
             </div>
 
             <div className="input-div-o">
-              <label htmlFor="">
-              Net Taxable Income
-              </label>
-              <input type="number" value={salary} onChange={handleSalary} name="" className="inputs" id="" />
+              <label htmlFor="">Net Taxable Income</label>
+              <input
+                type="number"
+                value={salary}
+                onChange={handleSalary}
+                name=""
+                className="inputs"
+                id=""
+              />
             </div>
 
             <div className="input-div">
@@ -498,18 +520,12 @@ function IncomeTax() {
                 id=""
               />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">TDS/TCS/MAT (AMT) Credit Utilized</label>
-              <input
-                type="number"
-                name=""
-                className="inputs"
-                id=""
-                readOnly
-              />
+              <input type="number" name="" className="inputs" id="" readOnly />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">Assessed Tax</label>
               <input
@@ -526,10 +542,11 @@ function IncomeTax() {
               <button className="calc_btn" onClick={handleHuf}>
                 Calculate
               </button>
-              <button className="reset_btn" onClick={handleReset}>Reset</button>
+              <button className="reset_btn" onClick={handleReset}>
+                Reset
+              </button>
             </div>
           </div>
-
         )}
 
         {component === "domestic" && (
@@ -551,10 +568,15 @@ function IncomeTax() {
             </div>
 
             <div className="input-div-o">
-              <label htmlFor="">
-              Net Taxable Income
-              </label>
-              <input type="number" value={salary} onChange={handleSalary} name="" className="inputs" id="" />
+              <label htmlFor="">Net Taxable Income</label>
+              <input
+                type="number"
+                value={salary}
+                onChange={handleSalary}
+                name=""
+                className="inputs"
+                id=""
+              />
             </div>
 
             <div className="input-div">
@@ -604,18 +626,12 @@ function IncomeTax() {
                 id=""
               />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">TDS/TCS/MAT (AMT) Credit Utilized</label>
-              <input
-                type="number"
-                name=""
-                className="inputs"
-                id=""
-                readOnly
-              />
+              <input type="number" name="" className="inputs" id="" readOnly />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">Assessed Tax</label>
               <input
@@ -632,10 +648,11 @@ function IncomeTax() {
               <button className="calc_btn" onClick={handleDom}>
                 Calculate
               </button>
-              <button className="reset_btn" onClick={handleReset}>Reset</button>
+              <button className="reset_btn" onClick={handleReset}>
+                Reset
+              </button>
             </div>
           </div>
-
         )}
 
         {component === "foreign" && (
@@ -657,10 +674,15 @@ function IncomeTax() {
             </div>
 
             <div className="input-div-o">
-              <label htmlFor="">
-              Net Taxable Income
-              </label>
-              <input type="number" value={salary} onChange={handleSalary} name="" className="inputs" id="" />
+              <label htmlFor="">Net Taxable Income</label>
+              <input
+                type="number"
+                value={salary}
+                onChange={handleSalary}
+                name=""
+                className="inputs"
+                id=""
+              />
             </div>
 
             <div className="input-div">
@@ -710,18 +732,12 @@ function IncomeTax() {
                 id=""
               />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">TDS/TCS/MAT (AMT) Credit Utilized</label>
-              <input
-                type="number"
-                name=""
-                className="inputs"
-                id=""
-                readOnly
-              />
+              <input type="number" name="" className="inputs" id="" readOnly />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">Assessed Tax</label>
               <input
@@ -738,14 +754,14 @@ function IncomeTax() {
               <button className="calc_btn" onClick={handleFor}>
                 Calculate
               </button>
-              <button className="reset_btn" onClick={handleReset}>Reset</button>
+              <button className="reset_btn" onClick={handleReset}>
+                Reset
+              </button>
             </div>
           </div>
-
         )}
 
-        
-{component === "firms" && (
+        {component === "firms" && (
           <div>
             <div className="input-div">
               <label htmlFor="">
@@ -764,10 +780,15 @@ function IncomeTax() {
             </div>
 
             <div className="input-div-o">
-              <label htmlFor="">
-              Net Taxable Income
-              </label>
-              <input type="number" value={salary} onChange={handleSalary} name="" className="inputs" id="" />
+              <label htmlFor="">Net Taxable Income</label>
+              <input
+                type="number"
+                value={salary}
+                onChange={handleSalary}
+                name=""
+                className="inputs"
+                id=""
+              />
             </div>
 
             <div className="input-div">
@@ -817,18 +838,12 @@ function IncomeTax() {
                 id=""
               />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">TDS/TCS/MAT (AMT) Credit Utilized</label>
-              <input
-                type="number"
-                name=""
-                className="inputs"
-                id=""
-                readOnly
-              />
+              <input type="number" name="" className="inputs" id="" readOnly />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">Assessed Tax</label>
               <input
@@ -845,13 +860,14 @@ function IncomeTax() {
               <button className="calc_btn" onClick={handleDom}>
                 Calculate
               </button>
-              <button className="reset_btn" onClick={handleReset}>Reset</button>
+              <button className="reset_btn" onClick={handleReset}>
+                Reset
+              </button>
             </div>
           </div>
-
         )}
-        
-{component === "llp" && (
+
+        {component === "llp" && (
           <div>
             <div className="input-div">
               <label htmlFor="">
@@ -870,10 +886,15 @@ function IncomeTax() {
             </div>
 
             <div className="input-div-o">
-              <label htmlFor="">
-              Net Taxable Income
-              </label>
-              <input type="number" value={salary} onChange={handleSalary} name="" className="inputs" id="" />
+              <label htmlFor="">Net Taxable Income</label>
+              <input
+                type="number"
+                value={salary}
+                onChange={handleSalary}
+                name=""
+                className="inputs"
+                id=""
+              />
             </div>
 
             <div className="input-div">
@@ -923,18 +944,12 @@ function IncomeTax() {
                 id=""
               />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">TDS/TCS/MAT (AMT) Credit Utilized</label>
-              <input
-                type="number"
-                name=""
-                className="inputs"
-                id=""
-                readOnly
-              />
+              <input type="number" name="" className="inputs" id="" readOnly />
             </div>
-            
+
             <div className="input-div">
               <label htmlFor="">Assessed Tax</label>
               <input
@@ -951,13 +966,73 @@ function IncomeTax() {
               <button className="calc_btn" onClick={handleDom}>
                 Calculate
               </button>
-              <button className="reset_btn" onClick={handleReset}>Reset</button>
+              <button className="reset_btn" onClick={handleReset}>
+                Reset
+              </button>
             </div>
           </div>
-
         )}
+
+        <div>
+          <p className="introduc">Advance Tax liability</p>
+          <div className="tax-initial">
+            <p className="heading-d">Description</p>
+            <p className="heading">Advance Tax liability</p>
+            <p className="detail-text-d">
+              Advance tax payable upto June 15, 2023 (Cumulative)
+            </p>
+            <p className="detail-text">{june}.00</p>
+            <p className="detail-text-d">
+              Advance tax payable upto September 15, 2023 (Cumulative)
+            </p>
+            <p className="detail-text">{sept}.00</p>
+            <p className="detail-text-d">
+              Advance tax payable upto December 15, 2023 (Cumulative)
+            </p>
+            <p className="detail-text">{dec}.00</p>
+            <p className="detail-text-d">
+              Advance tax payable upto March 15, 2024 (Cumulative)
+            </p>
+            <p className="detail-text">{result}.00</p>
+            <p className="detail-text-d">
+              Advance tax payable upto March 31, 2024 (Cumulative)
+            </p>
+            <p className="detail-text">{result}.00</p>
+          </div>
+        </div>
+        <div>
+          <p className="introduc">Advance Tax Installments</p>
+          <div className="tax-initial">
+            <p className="heading-d">Description</p>
+            <p className="heading"> Installment</p>
+            <p className="detail-text-d">
+              First installment payable for the period April 1, 2023 to June 15,
+              2023
+            </p>
+            <p className="detail-text">{june}.00</p>
+            <p className="detail-text-d">
+              Second installment payable for the period June 16, 2023 to
+              September 15, 2023
+            </p>
+            <p className="detail-text">{june * 2}.00</p>
+            <p className="detail-text-d">
+              Third installment payable for the period September 16, 2023 to
+              December 15, 2023
+            </p>
+            <p className="detail-text">{june * 2}.00</p>
+            <p className="detail-text-d">
+              Fourth installment payable for the period December 16, 2023 to
+              March 15, 2024
+            </p>
+            <p className="detail-text">{result - sum}.00</p>
+            <p className="detail-text-d">
+              Last installment payable for the period March 16, 2024 to March
+              31, 2024
+            </p>
+            <p className="detail-text">{0}.00</p>
+          </div>
+        </div>
       </div>
-      
     </div>
   );
 }
